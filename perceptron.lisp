6f4b2d92c6561701e025cabe3fdd1d36e6b65fce
Lucas Guerra Borges <lucasecp@MacBook-Pro-de-Lucas-2.local>
2016-10-05 16:13:02 -0300

    Code optimization

--- perceptron.lisp
+++ perceptron.lisp
@@ -1,4 +1,12 @@
 ;;Perceptrons 
+(declaim (optimize (speed 3) (debug 0) (safety 0) )
+	 (ftype (function (*) t ) check)
+	 (ftype (function (* *) t ) get-lst)
+	 (ftype (function (* &optional *) t) new-weight)
+	 (ftype (function (&optional *) t ) train )
+	 (ftype (function (*) t ) test ))
+	 
+
 (defparameter *outputs* '(1 1 -1 -1) )
 (defparameter *weight* '(0 0 0 0) ) ;;w1 w2 w3 w4
 (defparameter *b* 0 )
@@ -8,6 +16,7 @@
 
 ;;Check if the output is equal 
 (defun check (lst)
+  (declare (type LIST lst))
   (let ( (result 0 ))
     (setf result (reduce #'+ (mapcar #'* lst *weight*)))
     (if (>= (+  *b* result) 0 )
@@ -16,20 +25,28 @@
            
 ;;aux funcion for with-alpha 
 (defun get-lst (lst output )
+  (declare (type LIST lst)
+	   (type FIXNUM output))
   (loop for item in lst
 	collect
 	(* *a* output item)))
 
 ;;Calculate the new weight 
-(defun new-weight (lst &optional (output 1)) 
+(defun new-weight (lst &optional (output 1))
+  (declare (type LIST lst)
+	   (type FIXNUM output))
   (let  ((aux-lst (get-lst lst output)))
+    (declare (type LIST aux-lst))
     (setf *weight* (mapcar #'+ *weight* aux-lst))
     (setf *b* (+ *b* (*  *a* output)))
     (list *weight* *b*)))
 
 ;;Train the algorithm (and count the times needed to get 100% accurated)
 (defun train (&optional (count 0 ))
-  (let ( (changed 0 )) 
+  (declare (optimize (speed 3) (debug 0) (safety 0))
+	   (type FIXNUM count))
+  (let ( (changed 0 ))
+    (declare (type FIXNUM changed))
     (loop for i below 4
 	  do
 	  (progn (if (equal (nth i *outputs* ) (check (nth i *input* )))
